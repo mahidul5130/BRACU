48ba36ab9afcdc234f836c2669434347d6725533
Md. Mahidul Haque Sijan <54657768+mahidul5130@users.noreply.github.com>
2021-08-01 20:54:31 +0600

    Update LongMultiplication.py

--- BRACU Theory/CSE340/Multiplication/LongMultiplication/LongMultiplication.py
+++ BRACU Theory/CSE340/Multiplication/LongMultiplication/LongMultiplication.py
@@ -5,8 +5,8 @@ myTable = PrettyTable(["Iteration", "Multiplicand", "Multiplier", "Product"])
   
 
 
-Multiplicand = 28
-Multiplier = 3
+Multiplicand = 8
+Multiplier = 9
 product = 0
 
 Multiplicand_bit=8
@@ -28,11 +28,11 @@ while Multiplier!=0:
         myTable.add_row(["", f"{'0'*(Multiplicand_bit-len(str(bin(Multiplicand))[2:]))}{str(bin(Multiplicand))[2:]}", f"{'0'*(Multiplier_Bit-len(str(bin(Multiplier))[2:]))}{bin(Multiplier)[2:]}", f"{'0'*(Product_Bit-len(str(bin(product))[2:]))}{str(bin(product))[2:]}"])
 
 
+    Multiplicand = Multiplicand * 2
+    myTable.add_row([f"{i}", f"{'0'*(Multiplicand_bit-len(str(bin(Multiplicand))[2:]))}{str(bin(Multiplicand))[2:]}", f"{'0'*(Multiplier_Bit-len(str(bin(Multiplier))[2:]))}{bin(Multiplier)[2:]}", f"{'0'*(Product_Bit-len(str(bin(product))[2:]))}{str(bin(product))[2:]}"])
+    
     Multiplier = int(Multiplier/2)
     myTable.add_row(["", f"{'0'*(Multiplicand_bit-len(str(bin(Multiplicand))[2:]))}{str(bin(Multiplicand))[2:]}", f"{'0'*(Multiplier_Bit-len(str(bin(Multiplier))[2:]))}{bin(Multiplier)[2:]}", f"{'0'*(Product_Bit-len(str(bin(product))[2:]))}{str(bin(product))[2:]}"])
-    Multiplicand = Multiplicand * 2
-    
-    myTable.add_row([f"{i}", f"{'0'*(Multiplicand_bit-len(str(bin(Multiplicand))[2:]))}{str(bin(Multiplicand))[2:]}", f"{'0'*(Multiplier_Bit-len(str(bin(Multiplier))[2:]))}{bin(Multiplier)[2:]}", f"{'0'*(Product_Bit-len(str(bin(product))[2:]))}{str(bin(product))[2:]}"])
     
     myTable.add_row([f"{'-'*20}", f"{'-'*20}", f"{'-'*20}", f"{'-'*20}"])
     # print(str(bin(Multiplicand)), end=' ')
